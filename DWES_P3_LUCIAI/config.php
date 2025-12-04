e7d6897e93e8eeca1377695ebef105aeee5d05c0
luciaisabel21 <lucialiaisabel000@gmail.com>
2025-12-04 14:51:46 +0100

    Corrección de rutas para despliegue en Render

--- DWES_P3_LUCIAI/config.php
+++ DWES_P3_LUCIAI/config.php
@@ -1,7 +1,7 @@
 <?php
 // Configuración de rutas base
 define('BASE_PATH', __DIR__);
-define('BASE_URL', 'https://tfc-cmct.onrender.com/DWES_P3_LUCIAI');
+define('BASE_URL', 'https://tfc-cmct.onrender.com');
 
 // Configuración de la base de datos
 define('DB_HOST', getenv('DB_HOST') ?: 'dpg-d4om0ui4d50c73909keg-a');
